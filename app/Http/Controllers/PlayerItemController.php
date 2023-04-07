15c6943ee603a683e5b4a0f1a280e915c446fd5e
萩原正悟 <129708527+HagiwaraShogo@users.noreply.github.com>
2023-04-07 11:18:49 +0900

    addItemの作成 (#9)

---
+++ app/Http/Controllers/PlayerItemController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PlayerItem;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class PlayerItemController extends Controller
+{
+    
+    public function add(Request $request, $id)
+    {
+        
+        $all_id = PlayerItem::query()
+        ->where(['player_id' => $id, 'item_id' => $request->input('itemId')]);
+        
+        $num = $request->input('count');
+        
+        if ($all_id->exists()) {
+            $num += $all_id->value('count');
+            $all_id->update(['count'=>$num]);
+        }
+        else
+        {
+            PlayerItem::insertGetId(
+                ['player_id' => $id,
+                'item_id' => $request->input('itemId'),
+                'count' => $num]);
+        }
+
+        return new Response([
+            'itemId' => $request->input('itemId'),
+            'count' => $num]);
+            
+    }
+}
